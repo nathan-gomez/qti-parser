e170d263d585773661735d6c77843663a2731bbf
Nathan Gomez <gomez.fede98@gmail.com>
2024-07-14 19:03:23 -0400

    feat: basic parsing done

--- cmd/parse.go
+++ cmd/parse.go
@@ -1,12 +1,14 @@
 package cmd
 
 import (
+	"encoding/csv"
 	"encoding/xml"
 	"fmt"
 	"os"
 
-	"github.com/nathan-gomez/qti-parser/models"
 	"github.com/spf13/cobra"
+
+	"github.com/nathan-gomez/qti-parser/models"
 )
 
 var parseCmd = &cobra.Command{
@@ -22,22 +24,80 @@ func init() {
 
 func cmdFunc(cmd *cobra.Command, args []string) {
 	filePath := args[0]
-	fmt.Printf("%v", filePath)
+	var items models.QtiItem
+
+	err := parseXml(filePath, &items)
+	if err != nil {
+		panic(err)
+	}
+
+	err = writeCsv(&items)
+	if err != nil {
+		fmt.Printf("an error has ocurred: %s", err)
+		panic(err)
+	}
+}
+
+func parseXml(filePath string, qtiItems *models.QtiItem) error {
+	var err error
 
 	xmlFile, err := os.ReadFile(filePath)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
-	var items models.Item
-
-	err = xml.Unmarshal(xmlFile, &items)
+	err = xml.Unmarshal(xmlFile, qtiItems)
 	if err != nil {
-		fmt.Printf("error: %v", err)
-		panic(err)
+		return err
 	}
 
-	for _, v := range items.ResponseDeclaration.CorrectResponse {
-		fmt.Printf("\n MapKey: %v", v)
+	return nil
+}
+
+func writeCsv(qtiItem *models.QtiItem) error {
+	csvFile, err := os.Create("test.csv")
+	if err != nil {
+		return err
 	}
+	defer csvFile.Close()
+
+	csvWriter := csv.NewWriter(csvFile)
+	defer csvWriter.Flush()
+
+	headers, err := csvHeaders()
+	if err != nil {
+		return err
+	}
+
+	err = csvWriter.Write(headers)
+	if err != nil {
+		return err
+	}
+
+	// for _, item := range qtiItems {
+	csvString, err := toStringArr(qtiItem)
+	if err != nil {
+		return err
+	}
+
+	err = csvWriter.Write(csvString)
+	if err != nil {
+		return err
+	}
+	// }
+	return nil
+}
+
+func csvHeaders() ([]string, error) {
+	headers := []string{"Identifier", "Cardinality", "BaseType", "CorrectResponse"}
+	return headers, nil
+}
+
+func toStringArr(qtiItem *models.QtiItem) ([]string, error) {
+	// for _, item := range qtiItem.ResponseDeclaration {
+	// }
+	item := qtiItem.ResponseDeclaration[0]
+	row := []string{item.Identifier, item.Cardinality, item.BaseType, ""}
+
+	return row, nil
 }
--- models/qti-model.go
+++ models/qti-model.go
@@ -2,12 +2,23 @@ package models
 
 import "encoding/xml"
 
-type Item struct {
-	XMLName             xml.Name            `xml:"assessmentItem"`
-	ResponseDeclaration ResponseDeclaration `xml:"responseDeclaration"`
+type QtiItem struct {
+	XMLName             xml.Name              `xml:"assessmentItem"`
+	ResponseDeclaration []ResponseDeclaration `xml:"responseDeclaration"`
+	OutcomeDeclaration  []OutcomeDeclaration  `xml:"outcomeDeclaration"`
+}
+
+type OutcomeDeclaration struct {
+	Identifier   string   `xml:"identifier,attr"`
+	Cardinality  string   `xml:"cardinality,attr"`
+	BaseType     string   `xml:"baseType,attr"`
+	DefaultValue []string `xml:"defaultValue>value"`
 }
 
 type ResponseDeclaration struct {
+	Identifier      string   `xml:"identifier,attr"`
+	Cardinality     string   `xml:"cardinality,attr"`
+	BaseType        string   `xml:"baseType,attr"`
 	CorrectResponse []string `xml:"correctResponse>value"`
 	Mapping         Mapping  `xml:"mapping"`
 }
